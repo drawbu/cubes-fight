1b53c0f5820ab9d551e53041f2cf66878fc0da2b
Clément <clement2104.boillot@gmail.com>
2023-03-31 12:20:00 +0200

    :sparkles: Add a ConnectionManager to broadcast to all websockets

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from typing import Dict
+from typing import Dict, Union
 
 from fastapi import FastAPI, WebSocket
 from starlette.responses import HTMLResponse, FileResponse
@@ -7,8 +7,30 @@ from starlette.websockets import WebSocketDisconnect
 
 app = FastAPI()
 players: Dict[str, dict] = {}
-change_list = []
 DEFAULT_VALUES = {"x": 300, "y": 300, "angle": 0, "alive": True}
+
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: list[WebSocket] = []
+
+    async def connect(self, ws: WebSocket):
+        await ws.accept()
+        self.active_connections.append(ws)
+
+    def disconnect(self, ws: WebSocket):
+        self.active_connections.remove(ws)
+
+    async def broadcast(self, message: Union[str, dict]):
+        if isinstance(message, dict):
+            for connection in self.active_connections:
+                await connection.send_json(message)
+            return
+        for connection in self.active_connections:
+            await connection.send_text(message)
+
+
+manager = ConnectionManager()
 
 
 @app.get("/")
@@ -25,24 +47,13 @@ async def static_files(path: str):
 
 @app.websocket("/echo")
 async def websocket_endpoint(ws: WebSocket):
-    await ws.accept()
+    await manager.connect(ws)
     username = ""
     connected = False
     for player in players.values():
         await ws.send_json(player)
-    i = len(change_list)
     try:
         while True:
-            while i < len(change_list):
-                user_change = change_list[i]
-                i += 1
-                if user_change == username:
-                    continue
-                if user_change not in players:
-                    await ws.send_json({"username": user_change, "alive": False})
-                    continue
-                await ws.send_json(players[user_change])
-
             data = await ws.receive_json()
             if not data:
                 continue
@@ -56,9 +67,9 @@ async def websocket_endpoint(ws: WebSocket):
                 players[username] = {**DEFAULT_VALUES, **data}
             else:
                 players[username].update(data)
-            change_list.append(username)
-            i += 1
+            await manager.broadcast(players[username])
     except WebSocketDisconnect:
+        manager.disconnect(ws)
         if username:
             del players[username]
-            change_list.append(username)
+            await manager.broadcast({"username": username, "alive": False})
