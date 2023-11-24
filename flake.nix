4c9b24d75179d1b1fedb7d9005f1201311fe8b1e
Clément <clement2104.boillot@gmail.com>
2023-11-24 12:37:22 +0100

    Add Nix Flake

--- flake.nix
+++ flake.nix
@@ -1,16 +1,75 @@
 {
-  inputs = {
-    nixpkgs.url = "nixpkgs/nixos-23.05";
-    utils.url = "github:numtide/flake-utils";
-  };
+  outputs = { self, nixpkgs, }:
+    let
+      forAllSystems = function:
+        nixpkgs.lib.genAttrs [
+          "x86_64-linux"
+          "aarch64-linux"
+          "x86_64-darwin"
+          "aarch64-darwin"
+        ]
+          (system: function nixpkgs.legacyPackages.${system});
 
-  outputs = { self, nixpkgs, utils } @inputs:
-    utils.lib.eachDefaultSystem (system:
-      with import nixpkgs { inherit system; }; {
-        devShells.default = mkShell {
-          packages = [
-            python311
-          ];
-        };
+      py = pkgs: rec {
+        deps = p: with p; [ uvicorn ] ++ [ fastapi starlette gunicorn pydantic ];
+        env = pkgs.python3.withPackages deps;
+      };
+    in
+    {
+      devShells = forAllSystems (pkgs: {
+        default =
+          let
+            pyenv = (py pkgs).env;
+          in
+          pkgs.mkShell {
+            buildIputs = [ pyenv ] ++ [ pkgs.docker pkgs.docker-compose ];
+
+            shellHook = ''
+              echo "python env path -> ${pyenv}"
+            '';
+          };
       });
+
+      formatter = forAllSystems (pkgs: pkgs.nixpkgs-fmt);
+      packages = forAllSystems (pkgs: rec {
+        default = server;
+
+        server =
+          let pyenv = (py pkgs).env;
+          in pkgs.stdenvNoCC.mkDerivation {
+            name = "cubes-server";
+            version = "1.0.0";
+
+            src = pkgs.lib.cleanSource ./.;
+
+            buildPhase = ''
+              runHook preBuild
+
+              cat <<EOF > runner_script
+              #!${pkgs.runtimeShell}
+
+              ${pyenv}/bin/gunicorn -k uvicorn.workers.UvicornWorker --bind unix:\$1 -m 007 wsgi:app
+              EOF
+
+              runHook postBuild
+            '';
+
+            installPhase = ''
+              runHook preInstall
+
+              mkdir -p $out
+
+              cp wsgi.py $out
+              cp -r public $out
+              cp -r server $out
+
+              mkdir -p $out/bin
+              cp runner_script $out/bin/cubes-server
+              chmod +x $out/bin/cubes-server
+
+              runHook postInstall
+            '';
+          };
+      });
+    };
 }
